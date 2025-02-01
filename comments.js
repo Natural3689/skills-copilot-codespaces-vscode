799e25c8f795edf21b2be67d3702b1f62905d8d8
NIÁRA <relayed-aid.3o@icloud.com>
2025-02-01 22:10:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,32 +1,8 @@
 // create web server
+var express = require('express');
+var app = express();
 
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-var qs = require('querystring');
-var comments = [];
-
-http.createServer(function (req, res) {
-    var pathname = url.parse(req.url).pathname;
-    var query = url.parse(req.url, true).query;
-    if (pathname == '/') {
-        fs.readFile('./index.html', function (err, data) {
-            if (err) {
-                console.log(err);
-                res.end('server error');
-            } else {
-                res.writeHead(200, { 'Content-Type': 'text/html' });
-                res.end(data);
-            }
-        });
-    } else if (pathname == '/comment') {
-        var comment = query.comment;
-        comments.push(comment);
-        res.end('success');
-    } else if (pathname == '/getComments') {
-        var data = {
-            comments: comments
-        };
-        res.end(JSON.stringify(data));
-    }
-}).listen(3000, '
+// create comment list
+var comments = [
+    { name: 'John', email: 'john@example.com' }
+  ];
